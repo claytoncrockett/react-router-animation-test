0e582572ee14fb1adff232082694b46090aa3a69
Clayton Crockett <claytoncrockett@gmail.com>
2018-12-06 15:47:23 -0700

    background-colors

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "../styles.css";
 
 class Home extends React.Component {
   render() {
     return (
-      <div>
+      <div class="backgroundone">
         <p>hello</p>
         <Link to="/page">
           <h1>Click me for animation</h1>
--- src/Components/Page.js
+++ src/Components/Page.js
@@ -4,10 +4,10 @@ import { Link } from "react-router-dom";
 class Page extends React.Component {
   render() {
     return (
-      <div>
+      <div class="backgroundtwo">
         <h1>TEST</h1>
         <Link to="/">
-          <h1>Click me again for diff animation</h1>
+          <h1>Click me again for another animation</h1>
         </Link>
       </div>
     );
